55c78e447ebd883b8a9f643dddd30d202e8b62fc
Humdaan Mirza <mirzahumdaan786@gmail.com>
2024-12-14 09:29:51 +0530

    Last commit before Deployment

--- base/views.py
+++ base/views.py
@@ -4,6 +4,8 @@ from .models import Room
 # Create your views here.
 from .forms import RoomForm
 
+#coding logic page for allactions being taken on website
+
 
 def home(request):
     rooms=Room.objects.all()
@@ -12,9 +14,9 @@ def home(request):
 
 
 # rooms=[
-#     {'id':1, 'name':"Romm no 1 for ifykwyk"},
+#     {'id':1, 'name':"Romm no 1 for......"},
 #     {'id':2, 'name':'Only Studies'},
-#     {'id':3, 'name':'Keep studyn biches'},
+#     {'id':3, 'name':'Keep studyin...'},
 # ]
 # room template
 # we have pk ahic is passedinto function by ui and another list in db
@@ -26,20 +28,29 @@ def room(request,pk):
 
 # form view
 def createRoom(request):
-    form=RoomForm()
+    # empty obj or empty row of Room table 
+    form = RoomForm()
+    # this objs attributes will be filled by page's input fields
+    # Now when post api call is being made form's POST method next lines are being 
+    # executed
     if request.method == "POST":
         print(request.POST)
         # we got a dic of key list of values assosiated with it
+        # filling or form obj with that dictionaries values
         form = RoomForm(request.POST)
         if form.is_valid():
+            # check if valid GOOD Coding Practice
             form.save()
+            # save to db and redirect
             return redirect('home')
-    context ={'form':form}
+    context = {'form':form}
     return render(request,'base/room_form.html',context)
 
 # form view
 def updateRoom(request,pk):
+    # selection of alredy filled row of table
     room  = Room.objects.get(id=pk)
+    # slection of values filled in form
     form = RoomForm(instance=room)
     # we ahve selected a single row of room table by mathcing pk or id
     if request.method == "POST":
